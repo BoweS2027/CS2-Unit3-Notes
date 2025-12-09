b18d86524569619e9bb565fc4b70ac097365ba60
BoweS2027 <bowe.siegelson2027@gbwl.org>
2025-12-09 16:08:36 +0000

    3.3

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      //create a new string shortcut
+      String st = "Hello World";
+      //original string
+      String str = new String("goat");
+      //all cojects can be created using className objectName = new className
+      BuildABear kitty = new BuildABear("cat");
    }
 }
