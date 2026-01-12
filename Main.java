4c8f56d9a432ce3ad81b7c41a1143ab3e2cdde0a
BoweS2027 <bowe.siegelson2027@gbwl.org>
2026-01-12 14:41:10 +0000

    tyfh

--- Main.java
+++ Main.java
@@ -8,5 +8,19 @@ public class Main {
       //all cojects can be created using className objectName = new className
       BuildABear kitty = new BuildABear();
       System.out.println(kitty);
-   }
-}
+
+      // create a new object with our own values
+      // called the Constructor: ClassName(string, string, int)
+      BuildABear doggy = new BuildABear("cat", "Catterackanitic", 90);
+      //printing an object calls the classes toString() method
+      System.out.println(doggy);
+      // getters are non-void
+      System.out.println(kitty.getType());
+      double price = kitty.getPrice();
+      //setters are void
+      //calling void methods: objectName.methodName();
+      kitty.setName("doggully");
+      // setters (mutators) changed values. let'see the effects:
+
+   } //end main
+} // emd main class
